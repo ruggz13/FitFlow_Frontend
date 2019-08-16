19b8284731ccab8534a59b16b3f9564bff75be51
ruggz13 <nicolasruggieri7@gmail.com>
2019-08-16 17:26:12 -0400

    created addMyWorkout function

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
     console.log("Rob Loaded")
-    getAllWorkers()
+    getAllWorkouts()
     getForm()
 })
 
 function getForm(){
     const addWorkoutForm = document.getElementById("add-workout-form")
-    addWorkoutForm.addEventListener("submit", addWorkout)
+    addWorkoutForm.addEventListener("submit", addMyWorkout)
 }
 
 let workoutDivRowWrap = document.getElementById('pills-monday')
 
-function getAllWorkers(){
+function getAllWorkouts(){
     fetch('http://localhost:3000/workouts')
     .then(res => res.json())
     .then(res => res.forEach(showWorkouts))
@@ -20,8 +20,27 @@ function getAllWorkers(){
 
 
 function showWorkouts(workout){
-        debugger
    let workouttext = document.getElementById('monday1')
     workouttext.innerText = workout.name
 
+}
+
+function addMyWorkout(event){
+    debugger
+    event.preventDefault()
+    let data = {
+        name: event.target.querySelector("#workout-title").value,
+        description: event.target.querySelector("#workout-description").value,
+        video_url: event.target.querySelector("#youtube-url").value,
+        notes: event.target.querySelector("#workout-notes").value,
+        day_id: event.target.querySelector("#form-stacked-select").value
+    }
+    fetch("http://localhost:3000/workouts", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    .then(response => console.log(response))
 }
