4d72bd97d7e78e04f63ab5414329e9c283b34866
Trevahr Robertson <Trevahr@gmail.com>
2019-08-18 14:57:09 -0400

    some minor login functionality and scheduling added

--- index.js
+++ index.js
@@ -3,13 +3,36 @@ document.addEventListener("DOMContentLoaded", () => {
     getAllWorkouts()
     getForm()
 })
+let loginForm = document.getElementById('sign-in')
+loginForm.addEventListener('submit', login)
+
+function login(){
+    event.preventDefault()
+  let username = document.getElementById('username').value
+  fetch(`http://localhost:3000/login/${username}`)
+  .then(res => res.json())
+  .then(res => {
+      let displayUsername = document.getElementById('logged-in')
+      displayUsername.innerText = res.user.name
+      let pillDay = document.getElementById('pills-1')
+        pillDay.innerText = ""
+      res.workouts.forEach(showWorkouts)
+  })
+
+}
+
+
 
 function getForm(){
     const addWorkoutForm = document.getElementById("add-workout-form")
     addWorkoutForm.addEventListener("submit", addWorkout)
 }
 
-let workoutDivRowWrap = document.getElementById('pills-monday')
+function addWorkout(){
+
+}
+
+// let workoutDivRowWrap = document.getElementById('pills-monday')
 
 function getAllWorkouts(){
     fetch('http://localhost:3000/workouts')
@@ -17,10 +40,30 @@ function getAllWorkouts(){
     .then(res => res.forEach(showWorkouts))
 }
 
-
+let sundayButton = getElementById('pills-sunday-tab')
+sundayButton.addEventListener('click' renderDay)
 
 function showWorkouts(workout){
-   let workouttext = document.getElementById('')
-    workouttext.innerText = workout.name
+  
+//    let workoutTab = document.getElementById('pills-tabContent')
+   
+   let pillDay = document.getElementById('pills-1')
+
+    // pillDay.classList.add('tab-pane', 'fade', 'show', 'active')
+    // pillDay.id = `pills-monday`
+    // pillDay.role.add('tabpanel')
+   let rowWrap = document.createElement('div')
+   rowWrap.classList.add('row-wrap')
+   rowWrap.innerHTML=`
+   <div class="row bg-white p-4 align-items-center">
+     <div class="col-sm-3 col-md-3 col-lg-3"><h3 class="h5">${workout.name}</h3></div>
+     <div class="col-sm-3 col-md-3 col-lg-3"><span></span>Notes: ${workout.notes}</div>
+     <div class="col-sm-3 col-md-3 col-lg-3"><span class="icon-person mr-3"></span> David Holmes</div>
+     <div class="col-sm-3 col-md-3 col-lg-3 text-md-right"><a href="#" class="btn btn-primary pill px-4 mt-3 mt-md-0">Delete Workout</a></div>     
+   </div>`
+
+//    workoutTab.appendChild(pillDay)
+   pillDay.appendChild(rowWrap)
+    
 
 }
